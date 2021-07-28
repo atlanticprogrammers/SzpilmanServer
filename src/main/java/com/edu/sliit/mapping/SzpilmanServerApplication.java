81c9bba21175d7ba42986c74c97bf338c5fe9171
CMBKLAP0169 <cmbklap0169@CMBKLAP0169.local>
2021-07-28 19:52:19 +0530

    first commit

---
+++ src/main/java/com/edu/sliit/mapping/SzpilmanServerApplication.java
@@ -0,0 +1,16 @@
+package com.edu.sliit.mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzpilmanServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SzpilmanServerApplication.class, args);
+	}
+}
+
+/**
+ create database Thogakade;
+ use Thogakade;
+ */
---
+++ src/test/java/com/edu/sliit/mapping/SzpilmanServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.sliit.mapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzpilmanServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
